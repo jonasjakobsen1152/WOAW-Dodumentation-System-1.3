6c94f9d1302b1065f08d69e968fb15f48e74907c
Thomasmp00 <thomaxx00@gmail.com>
2023-04-26 11:37:34 +0200

    W rizz

---
+++ src/GUI/Controller/AdminController.java
@@ -0,0 +1,57 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+
+    public TableView tblUser;
+    public TableColumn clmUsername;
+    public TableColumn clmRole;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void handleOpenCreateUser(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/GUI/View/CreateUpdateUser.fxml"));
+        try {
+            AnchorPane pane = loader.load();
+
+            Stage dialogWindow = new Stage();
+            Scene scene = new Scene(pane);
+            dialogWindow.setScene(scene);
+            dialogWindow.show();
+        }
+         catch (IOException e) {
+            alertUser("Error: Could not open the create user window");
+        }
+    }
+
+    public void handleOpenUpdateUser(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteUser(ActionEvent actionEvent) {
+    }
+
+    private void alertUser(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(error);
+        alert.setHeaderText(error + "");
+        alert.showAndWait();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/GUI/VIEW/Login.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }catch (IOException e){
+            System.out.println("Could not start the application");
+        }
     }
 }
