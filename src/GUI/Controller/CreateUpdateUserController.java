d84181347bfc0381d477101648d11a0367227044
Thomasmp00 <thomaxx00@gmail.com>
2023-04-27 10:28:18 +0200

    DUB

--- src/GUI/Controller/CreateUpdateUserController.java
+++ src/GUI/Controller/CreateUpdateUserController.java
@@ -1,22 +1,35 @@
 package GUI.Controller;
 
+import GUI.MODEL.CreateUpdateUserModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class CreateUpdateUserController {
-
-    public CreateUpdateUserController(){
-        ArrayList<String> roleList = new ArrayList<>();
-        roleList.add("Admin");
-        //cbRole.setItems(roleList);
-    }
-
+public class CreateUpdateUserController implements Initializable {
     public TextField txtUsername;
     public TextField txtPassword;
-    public ChoiceBox cbRole;
+    @FXML
+    public ChoiceBox<String> cbRole;
+    public CreateUpdateUserModel createUpdateUserModel;
+
+    public CreateUpdateUserController(){
+
+        createUpdateUserModel = CreateUpdateUserModel.getInstance();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> availableChoices = FXCollections.observableArrayList("Admin", "Salesmen");
+        cbRole.setItems(availableChoices);
+    }
 
     public void handleCreateUser(ActionEvent actionEvent) {
         String username = txtUsername.getText();
@@ -26,4 +39,6 @@ public class CreateUpdateUserController {
 
     public void handleUpdateUser(ActionEvent actionEvent) {
     }
+
+
 }
