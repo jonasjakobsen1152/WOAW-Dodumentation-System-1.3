aa30fc02e857f06624b74dafbd0cc79aa742e8a5
Andreas Eriksen <pjuske.10@live.dk>
2023-04-27 10:36:06 +0200

    tech

---
+++ src/DAL/DB/TechnicianDAO_DB.java
@@ -0,0 +1,6 @@
+package DAL.DB;
+
+import DAL.ITechnicianDAO;
+
+public class TechnicianDAO_DB implements ITechnicianDAO {
+}
---
+++ src/GUI/MODEL/TechnicianModel.java
@@ -0,0 +1,4 @@
+package GUI.MODEL;
+
+public class TechnicianModel {
+}
