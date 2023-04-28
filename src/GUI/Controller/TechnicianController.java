26dac4eb3534bc994170f5d1f6e7023e76553b67
Andreas Eriksen <pjuske.10@live.dk>
2023-04-28 10:20:30 +0200

    lol

--- src/GUI/Controller/TechnicianController.java
+++ src/GUI/Controller/TechnicianController.java
@@ -1,4 +1,23 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class TechnicianController {
+    public void handleAddNote(ActionEvent actionEvent) {
+    }
+
+    public void handleUpdateNote(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteNote(ActionEvent actionEvent) {
+    }
+
+    public void HandleAddImage(ActionEvent actionEvent) {
+    }
+
+    public void HandleUpdateImage(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteImage(ActionEvent actionEvent) {
+    }
 }
