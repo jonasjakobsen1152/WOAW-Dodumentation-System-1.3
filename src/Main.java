422b61add390b0d2d3fceccd480553a0070bbd98
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-05-01 10:11:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Parent root;
         try {
             root = FXMLLoader.load(getClass().getResource("/GUI/VIEW/Login.fxml"));
