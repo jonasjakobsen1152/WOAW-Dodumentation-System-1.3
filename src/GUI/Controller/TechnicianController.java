0bb16e57958359f5bd8b334f58a23591dc742536
Andreas Eriksen <pjuske.10@live.dk>
2023-05-01 11:15:37 +0200

    k

--- src/GUI/Controller/TechnicianController.java
+++ src/GUI/Controller/TechnicianController.java
@@ -1,30 +1,20 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.SortEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class TechnicianController {
-    public TableView tblNotes;
-    public TableColumn clmNotes;
-    public TableView tblImages;
-    public TableColumn clmImages;
 
-    public void handleAddNote(ActionEvent actionEvent) {
+    public TableColumn clmTitleWork;
+
+    public void handleOpenDocumentation(ActionEvent actionEvent) {
     }
 
-    public void handleUpdateNote(ActionEvent actionEvent) {
+    public void handleFinishJob(ActionEvent actionEvent) {
     }
 
-    public void handleDeleteNote(ActionEvent actionEvent) {
-    }
-
-    public void HandleAddImage(ActionEvent actionEvent) {
-    }
-
-    public void HandleUpdateImage(ActionEvent actionEvent) {
-    }
-
-    public void handleDeleteImage(ActionEvent actionEvent) {
+    public void tblWork(SortEvent<TableView> tableViewSortEvent) {
     }
 }
---
+++ src/GUI/Controller/TechnicianJobController.java
@@ -0,0 +1,30 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TechnicianJobController {
+    public TableColumn clmImages;
+    public TableView tblImages;
+    public TableColumn clmNotes;
+    public TableView tblNotes;
+
+    public void handleDeleteImage(ActionEvent actionEvent) {
+    }
+
+    public void HandleUpdateImage(ActionEvent actionEvent) {
+    }
+
+    public void HandleAddImage(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteNote(ActionEvent actionEvent) {
+    }
+
+    public void handleUpdateNote(ActionEvent actionEvent) {
+    }
+
+    public void handleAddNote(ActionEvent actionEvent) {
+    }
+}
