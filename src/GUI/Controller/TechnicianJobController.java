dbd067fd47f77fe8ae188a135076d9abbe73a22f
Andreas Eriksen <pjuske.10@live.dk>
2023-05-11 10:17:33 +0200

    dfd

--- src/GUI/Controller/TechnicianJobController.java
+++ src/GUI/Controller/TechnicianJobController.java
@@ -1,11 +1,19 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
 
 public class TechnicianJobController {
     public TableColumn clmImages;
@@ -15,7 +23,19 @@ public class TechnicianJobController {
 
 
     public void handleAddDocumentation(ActionEvent actionEvent) {
-        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/DocumentationWindow.fxml"));
+            AnchorPane pane = loader.load();
+
+
+            Stage dialogWindow = new Stage();
+            Scene scene = new Scene(pane);
+            dialogWindow.setScene(scene);
+            dialogWindow.showAndWait();
+        }catch (IOException e){
+            e.printStackTrace();
+            alertUser("Could not open the image window");
+        }
     }
 
     public void handleUpdateDocumentation(ActionEvent actionEvent) {
@@ -31,6 +51,26 @@ public class TechnicianJobController {
     }
 
     public void HandleAddImage(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/AddImageWindow.fxml"));
+            AnchorPane pane = loader.load();
+
+
+            Stage dialogWindow = new Stage();
+            Scene scene = new Scene(pane);
+            dialogWindow.setScene(scene);
+            dialogWindow.showAndWait();
+        }catch (IOException e){
+            e.printStackTrace();
+            alertUser("Could not open the image window");
+        }
+    }
+
+    private void alertUser(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(error);
+        alert.setHeaderText(error + "");
+        alert.showAndWait();
     }
 
 }
