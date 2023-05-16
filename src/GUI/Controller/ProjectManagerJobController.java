bd6960e77cd7b9ea0ed484eabe6f708c1e717621
Andreas Eriksen <pjuske.10@live.dk>
2023-05-16 10:50:36 +0200

    k

--- src/GUI/Controller/ProjectManagerJobController.java
+++ src/GUI/Controller/ProjectManagerJobController.java
@@ -1,4 +1,54 @@
 package GUI.Controller;
 
-public class ProjectManagerJobController {
+import BE.Job;
+import BE.User;
+import GUI.MODEL.TechnicianJobModel;
+import GUI.MODEL.TechnicianModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ProjectManagerJobController implements Initializable {
+
+    @FXML
+    private TableView<Job> tblWork;
+    @FXML
+    private TableColumn clmTitleWork;
+    private TechnicianModel technicianModel;
+    private TechnicianJobModel technicianJobModel;
+    private Job selectedJob;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        technicianModel = TechnicianModel.getInstance();
+        try {
+            technicianJobModel = TechnicianJobModel.getInstance();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            alertUser("Could not get list");
+        }
+        tblWork.setOnMouseClicked(event -> {
+            selectedJob = tblWork.getSelectionModel().getSelectedItem();
+        });
+        showWork();
+    }
+
+    public void showWork(){
+        clmTitleWork.setCellValueFactory(new PropertyValueFactory<User,String>("title"));
+        tblWork.setItems(technicianModel.getWorkToBeViewed());
+    }
+
+    private void alertUser(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(error);
+        alert.setHeaderText(error + "");
+        alert.showAndWait();
+    }
 }
