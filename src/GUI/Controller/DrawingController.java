df6e62ab29e9a8480e9443a6939d504311864d57
Thomasmp00 <thomaxx00@gmail.com>
2023-05-16 11:03:41 +0200

    Save drawing

--- src/GUI/Controller/DrawingController.java
+++ src/GUI/Controller/DrawingController.java
@@ -1,16 +1,26 @@
 package GUI.Controller;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DrawingController {
-
+    
     @FXML
     private Canvas canvas;
     private GraphicsContext gc;
@@ -21,8 +31,31 @@ public class DrawingController {
         gc.setStroke(Color.BLACK);
     }
 
-    public void handleSetRed(ActionEvent actionEvent) {
-        gc.setStroke(Color.RED);
+    public void handleSaveDrawing(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Drawing");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG Image", "*.png"));
+
+        File selectedFile = fileChooser.showSaveDialog(null);
+        if (selectedFile != null) {
+            try {
+                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+                canvas.snapshot(null, writableImage);
+                RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                ImageIO.write(renderedImage, "png", selectedFile);
+                alertUser(Alert.AlertType.INFORMATION, "Saved", "The image has successfully saved");
+            } catch (IOException e) {
+                e.printStackTrace();
+                alertUser(Alert.AlertType.ERROR, "Error", "Could not save the image.");
+            }
+        }
+    }
+
+    private void alertUser(Alert.AlertType type, String title, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public void handleClearCanvas(ActionEvent actionEvent) {
@@ -37,5 +70,32 @@ public class DrawingController {
     public void handleDraw(MouseEvent event) {
         gc.lineTo(event.getX(), event.getY());
         gc.stroke();
+    }
+    public void handleSetRed(ActionEvent actionEvent) {
+        gc.setStroke(Color.RED);
+    }
+
+    public void handleSetBlue(ActionEvent actionEvent) {
+        gc.setStroke(Color.BLUE);
+    }
+
+    public void handleSetGreen(ActionEvent actionEvent) {
+        gc.setStroke(Color.GREEN);
+    }
+
+    public void handleSetBrown(ActionEvent actionEvent) {
+        gc.setStroke(Color.BROWN);
+    }
+
+    public void handleSetWhite(ActionEvent actionEvent) {
+        gc.setStroke(Color.WHITE);
+    }
+
+    public void handleSetBlack(ActionEvent actionEvent) {
+        gc.setStroke(Color.BLACK);
+    }
+
+    public void handleSetYellow(ActionEvent actionEvent) {
+        gc.setStroke(Color.YELLOW);
     }
 }
