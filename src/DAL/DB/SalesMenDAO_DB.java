0541aac3add124e02db7b16e1dc91a28c22beb3c
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-05-17 09:55:15 +0200

    Printer strategy

--- src/DAL/DB/SalesMenDAO_DB.java
+++ src/DAL/DB/SalesMenDAO_DB.java
@@ -1,7 +1,9 @@
 package DAL.DB;
 
 import BE.Customer;
+import BE.Documentation;
 import BE.Job;
+import BE.JobImage;
 import DAL.ISalesmenDAO;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
@@ -65,4 +67,56 @@ public class SalesMenDAO_DB implements ISalesmenDAO {
         }
         return jobs;
     }
+
+    @Override
+    public ArrayList<JobImage> getAllJobImages(Job selectedJob) throws SQLException {
+        try(Connection conn = databaseConnector.getConnection()){
+            ArrayList<JobImage> jobImages = new ArrayList<>();
+            String sql = "SELECT * FROM Image WHERE JobID =" +selectedJob.getId()+";";
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                int id = rs.getInt("ID");
+                String title = rs.getString("Title");
+                byte[] bytes = rs.getBytes("ImageData");
+                int jobId = rs.getInt("JobID");
+                String privacy = rs.getString("Private");
+
+                JobImage jobImage = new JobImage(id,title,bytes,jobId,privacy);
+                jobImages.add(jobImage);
+            }
+            return jobImages;
+        }catch (SQLException e){
+            throw new SQLException(e);
+        }
     }
+
+    @Override
+    public ArrayList<Documentation> getallDocuments(Job selectedJob) throws SQLException {
+        ArrayList<Documentation> documentations = new ArrayList<>();
+        try (Connection conn = databaseConnector.getConnection();
+             Statement stmt = conn.createStatement()){
+            String sql = "SElECT * FROM Document WHERE JobID ="+selectedJob.getId()+";";
+
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                int id = rs.getInt("ID");
+                String title = rs.getString("Title");
+                String publicNote = rs.getString("PublicNote");
+                String privateNote = rs.getString("PrivateNote");
+                int jobId = rs.getInt("JobID");
+
+                Documentation documentation = new Documentation(id,title,publicNote,privateNote,jobId);
+                documentations.add(documentation);
+            }
+            return documentations;
+        }catch (SQLException e){
+            throw new SQLException(e);
+        }
+    }
+}
--- src/DAL/ISalesmenDAO.java
+++ src/DAL/ISalesmenDAO.java
@@ -1,7 +1,9 @@
 package DAL;
 
 import BE.Customer;
+import BE.Documentation;
 import BE.Job;
+import BE.JobImage;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.sql.SQLException;
@@ -13,4 +15,8 @@ public interface ISalesmenDAO {
     ArrayList<Customer> getAllCustomers() throws SQLException;
 
     ArrayList<Job> getAllJobs(Customer selectedCustomer) throws SQLException;
+
+    ArrayList<JobImage> getAllJobImages(Job selectedJob) throws SQLException;
+
+    ArrayList<Documentation> getallDocuments(Job selectedJob) throws SQLException;
 }
