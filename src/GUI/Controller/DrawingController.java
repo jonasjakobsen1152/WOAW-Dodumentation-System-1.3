49669ce74fca38fe6f801779bff07a9b9d4358d6
Thomasmp00 <113883012+Thomasmp00@users.noreply.github.com>
2023-05-18 14:31:41 +0200

    --.,

--- src/GUI/Controller/DrawingController.java
+++ src/GUI/Controller/DrawingController.java
@@ -27,11 +27,20 @@ public class DrawingController {
 
     public void initialize() {
         gc = canvas.getGraphicsContext2D();
+        //Sets the size of the pen that is used to draw
         gc.setLineWidth(2);
+        //Sets the color of the pen to black
         gc.setStroke(Color.BLACK);
     }
 
+    /**
+     * The method is called when the save button is clicked
+     * This method is used to save the drawing as a png file.
+     * The file is saved in the folder that the user chooses
+     * @param actionEvent
+     */
     public void handleSaveDrawing(ActionEvent actionEvent) {
+        //Opens the windows explore and lets the user choose a place to save the drawn image.
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Save Drawing");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG Image", "*.png"));
@@ -39,9 +48,13 @@ public class DrawingController {
         File selectedFile = fileChooser.showSaveDialog(null);
         if (selectedFile != null) {
             try {
+                //Uses the writable image class to create a new image using the canvas size
                 WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+                //Takes a snapshot pf the image created
                 canvas.snapshot(null, writableImage);
+                //Converts the datatype and stores it into a image file
                 RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                //Saves the image as a png file in the selected folder.
                 ImageIO.write(renderedImage, "png", selectedFile);
                 alertUser(Alert.AlertType.INFORMATION, "Saved", "The image has successfully saved");
             } catch (IOException e) {
@@ -54,23 +67,41 @@ public class DrawingController {
     private void alertUser(Alert.AlertType type, String title, String message) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
-        alert.setContentText(message);
+        alert.setHeaderText(message);
         alert.showAndWait();
     }
 
+    /**
+     * This method is used to clear the whole canvas so the user can start over.
+     * The method is called when the user clicks on the clear button
+     */
     public void handleClearCanvas(ActionEvent actionEvent) {
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
+    /**
+     * This method is called when the user clicks inside the canvas
+     * The method is used to start drawing
+     * @param event
+     */
     public void handleStartDrawing(MouseEvent event) {
         gc.beginPath();
         gc.moveTo(event.getX(), event.getY());
     }
 
+    /**
+     * This method is used to handle drawing
+     * @param event
+     */
     public void handleDraw(MouseEvent event) {
         gc.lineTo(event.getX(), event.getY());
         gc.stroke();
     }
+
+    /**
+     * The methods under are used to set the color of the pen based on which button is clicked
+     * @param actionEvent
+     */
     public void handleSetRed(ActionEvent actionEvent) {
         gc.setStroke(Color.RED);
     }
